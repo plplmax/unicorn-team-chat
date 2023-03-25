f440e95b562e44bc8d31ac0ae66d37b8d1aba9d4
Maksim Ploski <50287455+plplmax@users.noreply.github.com>
2023-03-26 02:32:11 +0300

    feat: implement a header component (#22)

---
+++ frontend/src/components/common/HeaderItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="header">
+    <h2>🦄 Team Unicorns</h2>
+  </div>
+</template>
+
+<style scoped>
+.header {
+  padding: 2.4rem 0;
+  box-shadow: inset 0 -0.1rem 0 #e5e5ea;
+}
+
+.header h2 {
+  font-size: 1.4rem;
+  text-align: center;
+}
+</style>
