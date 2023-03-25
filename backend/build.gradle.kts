3e7cf61a9efbcdbf540b2ba26d81dc1509f789f8
Maksim Ploski <50287455+plplmax@users.noreply.github.com>
2023-03-25 17:15:20 +0300

    init: initialize a backend part

---
+++ backend/build.gradle.kts
@@ -0,0 +1,32 @@
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+    kotlin("jvm") version "1.8.10"
+    id("io.ktor.plugin") version "2.2.4"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
+}
+
+group = "com.github.plplmax.chat"
+version = "0.0.1"
+
+application {
+    mainClass.set("com.github.plplmax.chat.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core:$ktor_version")
+    implementation("io.ktor:ktor-server-websockets:$ktor_version")
+    implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+    implementation("io.ktor:ktor-server-netty:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+}
---
+++ backend/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "com.github.plplmax.chat.unicorn-team-chat"
