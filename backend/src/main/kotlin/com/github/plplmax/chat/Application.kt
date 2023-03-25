2f20cb6c8c44ffe70d82387c27446994a29165fb
Maksim Ploski <50287455+plplmax@users.noreply.github.com>
2023-03-25 17:44:40 +0300

    init: initialize a backend part (#12)

---
+++ backend/src/main/kotlin/com/github/plplmax/chat/Application.kt
@@ -0,0 +1,19 @@
+package com.github.plplmax.chat
+
+import com.github.plplmax.chat.plugins.configureRouting
+import com.github.plplmax.chat.plugins.configureSerialization
+import com.github.plplmax.chat.plugins.configureSockets
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "localhost", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureSockets()
+    configureSerialization()
+    configureRouting()
+}
