9fd39831023330bc4cc96f2c149c8e32924001cb
Maksim Ploski <50287455+plplmax@users.noreply.github.com>
2023-04-16 22:13:38 +0300

    feat: implement an input component

---
+++ frontend/src/components/common/InputItem.vue
@@ -0,0 +1,60 @@
+<script setup>
+defineProps({
+  id: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    default: 'text'
+  },
+  label: {
+    type: String,
+    required: true
+  },
+  error: {
+    type: String,
+    default: ''
+  },
+  modelValue: {}
+})
+defineEmits(['update:modelValue'])
+</script>
+
+<template>
+  <div>
+    <label :for="id">{{ label }}</label>
+    <input
+      :id="id"
+      :name="name"
+      :type="type"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+    <p v-if="error" class="error">{{ error }}</p>
+  </div>
+</template>
+
+<style scoped>
+label {
+  font-size: 2em;
+  display: block;
+}
+
+input {
+  width: 100%;
+  height: 3.2rem;
+  margin-top: 0.6em;
+  border: 1px solid #818182;
+}
+
+.error {
+  color: red;
+  font-size: 1.2em;
+  margin-top: 0.6em;
+}
+</style>
