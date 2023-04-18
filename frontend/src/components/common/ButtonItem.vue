edc1b7181c95f9cfa9331bfbd7902d9123f30c34
Maksim Ploski <50287455+plplmax@users.noreply.github.com>
2023-04-18 12:14:58 +0300

    feat: implement a button component (#24)

---
+++ frontend/src/components/common/ButtonItem.vue
@@ -0,0 +1,31 @@
+<script setup>
+defineProps({
+  type: {
+    type: String,
+    default: 'button'
+  },
+  text: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <button :type="type">{{ text }}</button>
+</template>
+
+<style scoped>
+button {
+  font-family: 'Abel', sans-serif;
+  font-size: 2rem;
+  color: var(--color-on-primary);
+  background-color: var(--color-primary);
+  padding: 0.2rem 3.5rem;
+  border: 0;
+}
+
+button:hover {
+  cursor: pointer;
+}
+</style>
---
+++ frontend/src/components/common/ButtonWrapper.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="form-button" v-bind="$attrs">
+    <slot name="button" />
+  </div>
+</template>
+
+<style scoped>
+.form-button {
+  width: 11.1rem;
+  margin: 2.5em auto 0 auto;
+}
+</style>
+<script setup></script>
