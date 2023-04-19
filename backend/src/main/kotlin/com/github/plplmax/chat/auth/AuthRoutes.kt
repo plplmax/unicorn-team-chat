d04d2e8c50c91c01663352cc3965b93ff5220946
Maksim Ploski <50287455+plplmax@users.noreply.github.com>
2023-04-19 19:40:11 +0300

    puzzle: add the puzzle for authentication routes

---
+++ backend/src/main/kotlin/com/github/plplmax/chat/auth/AuthRoutes.kt
@@ -0,0 +1,14 @@
+package com.github.plplmax.chat.auth
+
+import io.ktor.server.routing.*
+
+/**
+ * @todo #5:30m Implement authentication route. Should be ability
+ * to log in with username and password. The raw password that passed
+ * from a user should be hashed and compared with the hashed password
+ * from the database. If the hashed passwords are equal than we should
+ * provide the user some token/session identifier to authenticate the
+ * following requests.
+ */
+fun Route.authRoutes() {
+}
