f71e1fbec068584494f281d6d16d0ba62dee7dae
Maksim Ploski <50287455+plplmax@users.noreply.github.com>
2023-04-19 19:45:18 +0300

    puzzle: fix puzzle description for the authentication routes

--- backend/src/main/kotlin/com/github/plplmax/chat/auth/AuthRoutes.kt
+++ backend/src/main/kotlin/com/github/plplmax/chat/auth/AuthRoutes.kt
@@ -3,7 +3,7 @@ package com.github.plplmax.chat.auth
 import io.ktor.server.routing.*
 
 /**
- * @todo #5:30m Implement authentication route. Should be ability
+ * @todo #5:30m Implement authentication routes. Should be ability
  * to log in with username and password. The raw password that passed
  * from a user should be hashed and compared with the hashed password
  * from the database. If the hashed passwords are equal than we should
