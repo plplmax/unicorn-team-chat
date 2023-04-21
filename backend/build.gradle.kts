17d2942d8af3b73437ba6345cf9121e6663941fe
Maksim Ploski <50287455+plplmax@users.noreply.github.com>
2023-04-21 22:05:53 +0300

    feat: implement the JWT authentication (#39)

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -28,6 +28,8 @@ repositories {
 dependencies {
     implementation("io.ktor:ktor-server-core:$ktor_version")
     implementation("io.ktor:ktor-server-websockets:$ktor_version")
+    implementation("io.ktor:ktor-server-auth:$ktor_version")
+    implementation("io.ktor:ktor-server-auth-jwt:$ktor_version")
     implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
     implementation("io.ktor:ktor-server-netty:$ktor_version")
