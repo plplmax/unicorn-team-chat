56e7e80890fea367422efebd9338198f5d425fab
Maksim Ploski <50287455+plplmax@users.noreply.github.com>
2023-04-23 19:46:07 +0300

    feat: implement message history at frontend

---
+++ frontend/src/components/chat/MessageItem.vue
@@ -0,0 +1,95 @@
+<script setup>
+defineProps({
+  self: {
+    type: Boolean,
+    default: false
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  message: {
+    type: String,
+    required: true
+  },
+  time: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div :class="{ message: true, 'message-left': !self, 'message-right': self }">
+    <p v-if="!self" class="author">{{ author }}</p>
+    <div class="content-wrapper">
+      <p class="content">{{ message }}</p>
+      <p class="time">{{ time }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.message {
+  display: flex;
+  position: relative;
+  max-width: 26rem;
+  width: fit-content;
+  margin-top: 1.2rem;
+  padding: 0.4rem 0.6rem 0.5rem 0.8rem;
+  font-size: 1.4rem;
+  border-radius: 0 0.6rem 0.6rem 0.6rem;
+  /* @todo #45:15m Replace background-color value with the constant */
+  background-color: #f2f2f7;
+  overflow-wrap: anywhere;
+}
+
+.content {
+  padding-bottom: 2rem;
+}
+
+.time {
+  min-width: fit-content;
+  margin-top: auto;
+  padding-left: 0.8rem;
+  text-align: right;
+}
+
+.content-wrapper {
+  display: flex;
+  margin-top: 0.4rem;
+}
+
+.message-right {
+  background-color: var(--color-primary);
+  color: var(--color-on-primary);
+  margin-left: auto;
+  border-radius: 0.6rem 0 0.6rem 0.6rem;
+}
+
+.message-left {
+  flex-direction: column;
+}
+
+.message-left::before {
+  content: '';
+  position: absolute;
+  right: 100%;
+  top: 0;
+  width: 0;
+  height: 0;
+  border-right: 1.5rem solid #f2f2f7;
+  border-bottom: 1.5rem solid transparent;
+}
+
+.message-right::after {
+  content: '';
+  position: absolute;
+  left: 100%;
+  top: 0;
+  width: 0;
+  height: 0;
+  border-right: 1.5rem solid transparent;
+  border-top: 1.5rem solid var(--color-primary);
+}
+</style>
---
+++ frontend/src/views/ChatView.vue
@@ -0,0 +1,32 @@
+<script setup>
+import MessageItem from '@/components/chat/MessageItem.vue'
+</script>
+
+<template>
+  <div class="message-container">
+    <MessageItem
+      author="Janet"
+      message="That works. I was actually planning to get a smoothie anyways 👍"
+      time="12:03 PM"
+    />
+    <MessageItem :self="true" author="Maks" message="Agreed" time="11:52 AM" />
+    <MessageItem author="Aubrey" message="I was thinking the cafe downtown" time="11:45 AM" />
+    <MessageItem :self="true" author="Maks" message="I am down for whatever!" time="11:36 AM" />
+    <MessageItem author="Jav" message="I’m down! Any ideas??" time="11:35 AM" />
+    <MessageItem :self="true" author="Maks" message="Anyone on for lunch today" time="11:31 AM" />
+    <MessageItem :self="true" author="Maks" message="Hi team 👋" time="11:31 AM" />
+  </div>
+</template>
+
+<style scoped>
+.message-container {
+  display: flex;
+  flex-direction: column-reverse;
+  max-width: 60rem;
+  height: calc(100vh - 6.65rem);
+  margin: auto;
+  padding: 2.5rem;
+  overflow: auto;
+  box-shadow: 0 0 5rem #e5e5ea;
+}
+</style>
