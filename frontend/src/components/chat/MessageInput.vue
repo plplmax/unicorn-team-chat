6bede49301e9bc3c37d2c6ac38e0bf793b5fa073
Maksim Ploski <50287455+plplmax@users.noreply.github.com>
2023-05-01 09:45:16 +0300

    feat: implement sending messages at frontend (#57)

---
+++ frontend/src/components/chat/MessageInput.vue
@@ -0,0 +1,41 @@
+<script setup>
+import IconSend from '../icons/IconSend.vue'
+
+defineProps({
+  modelValue: {},
+  send: {
+    type: Function,
+    required: true
+  }
+})
+defineEmits(['update:modelValue'])
+</script>
+
+<template>
+  <div class="send-message-container">
+    <input
+      :value="modelValue"
+      placeholder="Start typing..."
+      @input="$emit('update:modelValue', $event.target.value)"
+      @keyup.enter="send()"
+    />
+    <a @click.prevent="send()" href="#"><IconSend /></a>
+  </div>
+</template>
+
+<style scoped>
+input {
+  width: 100%;
+  font-size: 1.4rem;
+  padding: 1.4rem 4.4rem 1.4rem 5.2rem;
+  box-shadow: inset 0 0.1rem 0 #e5e5ea;
+  border: unset;
+}
+
+a {
+  position: absolute;
+  padding: 1.4rem 1.7rem 1.4rem 0;
+  right: 0;
+  cursor: pointer;
+}
+</style>
--- frontend/src/services/message.service.js
+++ frontend/src/services/message.service.js
@@ -1,15 +1,63 @@
 import axios from '@/axios'
 
+let socket
+let timeoutId
+
+export const connect = (token, added, edited, deleted) => {
+  if (socket) return
+  socket = new WebSocket('ws://localhost:8080/message')
+  socket.addEventListener('open', () => {
+    console.log('WebSocket connection opened')
+    socket.send(token)
+  })
+  socket.addEventListener('message', (event) => {
+    console.log('Received message:', event.data)
+    const parsedData = JSON.parse(event.data)
+    const type = parsedData.type
+    if (type === 'add') {
+      added({
+        ...parsedData.data,
+        date: toZonedTime(parsedData.data.date)
+      })
+    } else {
+      console.error('undefined type of message: ' + type)
+    }
+  })
+  socket.addEventListener('close', () => {
+    console.log('WebSocket connection closed')
+    if (!socket) return
+    socket = null
+    timeoutId = setTimeout(() => {
+      console.log('Reconnecting...')
+      connect(token, added, edited, deleted)
+    }, 5000)
+  })
+}
+
+export const add = (message) => {
+  if (!socket || socket.readyState !== WebSocket.OPEN) return false
+  socket.send(JSON.stringify({ type: 'add', message }))
+  return true
+}
+
+export const disconnect = () => {
+  console.log('Disconnect initiated by the user')
+  if (socket) socket.close()
+  socket = null
+  clearTimeout(timeoutId)
+}
+
 export const getMessages = () => {
   return axios.get('/message').then((response) => {
-    return response.data.map((message) => {
-      const date = new Date(message.date)
-      const zonedDate = new Date(date.getTime() - date.getTimezoneOffset() * 60 * 1000)
-      const dateString = zonedDate.toLocaleTimeString('en-us', {
-        hour: '2-digit',
-        minute: '2-digit'
-      })
-      return { ...message, date: dateString }
-    })
+    return response.data.map((message) => ({ ...message, date: toZonedTime(message.date) }))
+  })
+}
+
+export const toZonedTime = (dateString) => {
+  const date = new Date(dateString)
+  const zonedDate = new Date(date.getTime() - date.getTimezoneOffset() * 60 * 1000)
+  return zonedDate.toLocaleTimeString('en-us', {
+    hour: '2-digit',
+    minute: '2-digit'
   })
 }
