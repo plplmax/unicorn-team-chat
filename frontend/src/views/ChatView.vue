3613c016973939f52e12de58e91d4d93e8750b58
Maksim Ploski <50287455+plplmax@users.noreply.github.com>
2023-05-02 00:14:10 +0300

    feat: implement editing messages

--- frontend/src/views/ChatView.vue
+++ frontend/src/views/ChatView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onBeforeMount, onBeforeUnmount, ref } from 'vue'
+import { onBeforeMount, onBeforeUnmount, onMounted, onUnmounted, ref } from 'vue'
 import * as messageService from '@/services/message.service'
 import MessageItem from '@/components/chat/MessageItem.vue'
 import MessageInput from '@/components/chat/MessageInput.vue'
@@ -7,9 +7,15 @@ import { useToast } from 'vue-toastification'
 import 'vue-toastification/dist/index.css'
 
 const toaster = useToast()
+const userId = parseInt(localStorage.getItem('userId'))
+
 const messages = ref([])
 const messageForSending = ref('')
-const userId = parseInt(localStorage.getItem('userId'))
+const currentEditingMessage = ref(0)
+
+const showMenu = ref(false)
+const position = ref({ left: 0, top: 0 })
+
 const sendMessage = () => {
   const message = messageForSending.value.trim()
   if (!message) return
@@ -17,17 +23,50 @@ const sendMessage = () => {
   if (added) messageForSending.value = ''
 }
 
+const updateMessage = (message) => {
+  const trimmed = message.trim()
+  if (!trimmed) return
+  const updated = messageService.update(currentEditingMessage.value, trimmed)
+  if (updated) stopEditingMessage()
+}
+
+const startEditingMessage = () => {
+  messages.value.find((value) => value.id === currentEditingMessage.value).editable = true
+}
+
+const stopEditingMessage = () => {
+  messages.value.find((value) => value.id === currentEditingMessage.value).editable = false
+}
+const showContextMenu = (event, messageId, self) => {
+  if (!self) return
+  currentEditingMessage.value = messageId
+  position.value.left = event.clientX
+  position.value.top = event.clientY - 50
+  showMenu.value = true
+}
+
+const hideContextMenu = () => (showMenu.value = false)
+
 onBeforeMount(() => {
   messageService
     .getMessages()
     .then((response) => (messages.value = response))
     .then(() => {
       const token = localStorage.getItem('token')
-      messageService.connect(token, (message) => messages.value.unshift(message))
+      messageService.connect(
+        token,
+        (message) => messages.value.unshift(message),
+        (message) => {
+          const index = messages.value.findIndex((value) => value.id === message.id)
+          messages.value.splice(index, 1, message)
+        }
+      )
     })
     .catch(() => toaster.error('Cannot load history. Reload the page'))
 })
+onMounted(() => document.addEventListener('click', hideContextMenu))
 onBeforeUnmount(() => messageService.disconnect())
+onUnmounted(() => document.removeEventListener('click', hideContextMenu))
 </script>
 
 <template>
@@ -35,13 +74,25 @@ onBeforeUnmount(() => messageService.disconnect())
     <MessageItem
       v-for="message in messages"
       :key="message.id"
+      v-model:editable="message.editable"
       :author="message.sender"
       :message="message.message"
+      :on-edit="updateMessage"
       :self="message.senderId === userId"
       :time="message.date"
+      @contextmenu.prevent="showContextMenu($event, message.id, message.senderId === userId)"
     />
     <MessageInput v-model="messageForSending" :send="sendMessage" class="message-input" />
   </div>
+  <ul
+    v-show="showMenu"
+    id="myContextMenu"
+    :style="{ left: position.left + 'px', top: position.top + 'px' }"
+    class="context-menu"
+  >
+    <li @click="startEditingMessage()">Edit</li>
+    <li>Delete</li>
+  </ul>
 </template>
 
 <style scoped>
@@ -61,5 +112,30 @@ onBeforeUnmount(() => messageService.disconnect())
   left: 0;
   right: 0;
   bottom: 0;
+}
+
+.context-menu {
+  position: absolute;
+  background-color: white;
+  list-style: none;
+  box-shadow: 0 0 5rem #e5e5ea;
+  z-index: 100;
+  font-size: 1.8rem;
+}
+
+ul {
+  padding: 0;
+}
+
+li {
+  padding: 5px 10px;
+}
+
+li:hover {
+  background-color: #e5e5ea;
+}
+
+.context-menu li:hover {
+  cursor: pointer;
 }
 </style>
