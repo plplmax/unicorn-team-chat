1cf5a469bf609c25af556aa5cb7a1cf62f6d3043
Maksim Ploski <50287455+plplmax@users.noreply.github.com>
2023-05-01 12:28:32 +0300

    feat: implement login and chat guards

--- backend/src/main/kotlin/com/github/plplmax/chat/plugins/Routing.kt
+++ backend/src/main/kotlin/com/github/plplmax/chat/plugins/Routing.kt
@@ -3,13 +3,16 @@ package com.github.plplmax.chat.plugins
 import com.github.plplmax.chat.auth.authRoutes
 import com.github.plplmax.chat.message.messageRoutes
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respondText("Hello World!")
+        authenticate {
+            get("/") {
+                call.respondText("Hello World!")
+            }
         }
         authRoutes()
         messageRoutes()
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,10 +6,25 @@ import LoginView from '@/views/LoginView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import ChatView from '@/views/ChatView.vue'
 import VueToastificationPlugin from 'vue-toastification'
+import axios from '@/axios'
 
 const routes = [
-  { path: '/', component: LoginView },
-  { path: '/chat', component: ChatView }
+  {
+    path: '/',
+    component: LoginView,
+    beforeEnter: () =>
+      axios
+        .get('/')
+        .then(() => '/chat')
+        .catch(() => true)
+  },
+  {
+    path: '/chat',
+    component: ChatView,
+    beforeEnter: () => {
+      if (!localStorage.getItem('token')) return '/'
+    }
+  }
 ]
 
 const router = createRouter({ history: createWebHistory(), routes })
